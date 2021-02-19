e5814a215a63bf55d3ff3bee8fa68e4c0921a908
kpilarski <konrad.pilarski@gmail.com>
2021-02-19 20:55:35 +0100

    new feature: function that process song data
    adding to the main file with ETL process function that is handling
    processing song data

--- etl.py
+++ etl.py
@@ -23,22 +23,24 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
+    song_data = os.path.join(input_data, 'song_data', '*', '*', '*')
     
     # read song data file
-    df = 
+    df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table = 
+    songs_table = df.select(['song_id', 'title', 'artist_id', 'year', 'duration'])
     
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.parquet(os.path.join(output_data, 'songs'), partitionBy=['year', 'artist_id'])
 
     # extract columns to create artists table
-    artists_table = 
+    columns = ['artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']
+    columns = [col + ' as ' + col.replace('artist_', '') for col in columns]
+    artists_table = df.selectExpr('artist_id', *columns)
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write.parquet(os.path.join(output_data, 'artists'))
 
 
 def process_log_data(spark, input_data, output_data):
